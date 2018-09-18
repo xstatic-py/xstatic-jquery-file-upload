289d33c2e6cfdc6a652da9b6a8dd49ffe0eb0104
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2018-09-19 00:35:52 +0200

    updated jquery-file-upload files to 9.7.0, update xstatic metadata
    this work was done in July 2014, but rested uncommited in my workdir. :|

--- xstatic/pkg/jquery_file_upload/__init__.py
+++ xstatic/pkg/jquery_file_upload/__init__.py
@@ -11,9 +11,9 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '4.4' # version of the packaged files, please use the upstream
-                # version number
-BUILD = '2' # our package build number, so we can release new builds
+VERSION = '9.7.0' # version of the packaged files, please use the upstream
+                  # version number
+BUILD = '1' # our package build number, so we can release new builds
             # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
